e5384c19c79855f7cda49dd98a17b8b1f7f98e53
Nalin-khanna <nalinkhanna17@gmail.com>
2025-08-24 11:35:03 +0530

    seed issues

---
+++ prisma/seed_issues.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma";
+import { Octokit } from "@octokit/rest";
+
+const prisma = new PrismaClient();
+
+const octokit = new Octokit({ 
+    auth: process.env.GITHUB_TOKEN,
+});
+
+async function seedIssues(){
+    const repos = await prisma.repo.findMany(
+        {
+            include : {
+                organization : true
+            }
+        }
+    );
+    await Promise.all(repos.map(async (repo)=> {
+        try{
+            const issues = await octokit.rest.issues.listForRepo(
+                {
+                    state : "open",
+                    repo : repo.name,
+                    owner : repo.organization.githubUrl!.replace("https://github.com/", "")
+                }
+            )
+            await prisma.issue.createMany({
+                data : issues.data.map((issue:any)=> ({
+                    title : issue.title,
+                    number : issue.number,
+                    createdAt : issue.created_at,
+                    updatedAt : issue.updated_at,
+                    url : issue.html_url,
+                    repoId : repo.id,
+                    assigned : issue.assignee ? true : false,
+                    authorLogin : issue.user.login
+                }))
+            })
+        }
+        catch(e){
+            console.error(e);
+        }
+    }))
+}
+
+seedIssues();
